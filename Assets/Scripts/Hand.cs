185704f4a46764e55cbf612121098a3117eb43e8
gws1017 <gws1017@naver.com>
2023-12-19 16:07:42 +0900

    플레이어 애니메이션 추가
    - 플레이어의 손을 부착하고 공격, 무기장착, idle, walk , run 애니메이션을 추가했습니다.
    - 공격 애니메이션은 코루틴을 통해 처리되며 애니메이션이 재생되면 선딜레이 시간이 지나고 RayCast를 이용해 충돌 판정을 확인하고, 정해진 시간(attackDelay)가 지나면 충돌 판정을 멈추고 후딜레이(RecoveryTIme) 시간이 지난 후에 공격 코루틴 전체가 종료됩니다.

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public string WeaponName;
+    public float range;
+    public int damage;
+    public float workSpeed;
+    public float attackRecoveryTime; //후딜레이
+    public float attackFrontDelay; //선딜레이
+    public float attackDelay; //공격판정
+
+    public Animator anim;
+    
+
+}
---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    private Hand currentWeapon;
+
+    private bool isAttack = false;
+    private bool isSwing = false;
+
+    private RaycastHit hitInfo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if(!isAttack)
+            {
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        currentWeapon.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentWeapon.attackFrontDelay);
+        //충돌 판정 활성화
+        isSwing = true;
+        StartCoroutine(HitCoroutine());
+
+        yield return new WaitForSeconds(currentWeapon.attackDelay);
+        //충돌 판정 비활성화
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentWeapon.attackRecoveryTime);
+        isAttack = false;
+    }
+
+    IEnumerator HitCoroutine()
+    {
+        while(isSwing)
+        {
+            if(CheckObject())
+            {
+                isSwing = false;
+                Debug.Log(hitInfo.transform.name);
+            }
+            yield return null;
+            
+        }
+    }
+
+    private bool CheckObject()
+    {
+        if(Physics.Raycast(transform.position,transform.forward, out hitInfo,currentWeapon.range)) 
+        {
+            return true;
+        }
+        return false;
+    }
+}
